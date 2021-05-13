14e59d9750f9b1b64f928ce317984c8a67bd1d9f
chouquan <72334470+chouquan@users.noreply.github.com>
2021-05-13 23:41:22 +0100

    111

---
+++ js/myfullpagejs_story.js
@@ -0,0 +1,19 @@
+
+    $(function(){
+        $('#fullpage').fullpage({
+            /*设置每一屏的背景颜色 */
+            sectionsColor:['#E0EBE7',"#81CDBD",'#55C4C7','#7DACBF','#E0EBE7',"#81CDBD",'#7DACBF','#55C4C7','#E0EBE7',"#81CDBD",'#55C4C7','#7DACBF','#E0EBE7',"#81CDBD",'#55C4C7'],
+            /*显示右侧的导航原点*/
+            navigation:true,
+            /*弹跳*/
+            easingcss3:'cubic-bezier(0.175,0.885,0.320,1.05)',
+
+
+
+        });
+    })
+
+
+
+
+
