f7e7fb73b5475ade54fc5142c168c55e33b363f4
chouquan <72334470+chouquan@users.noreply.github.com>
2021-05-16 00:20:35 +0100

    111

--- js/myfullpagejs_team.js
+++ js/myfullpagejs_team.js
@@ -2,11 +2,11 @@
     $(function(){
         $('#fullpage').fullpage({
             /*设置每一屏的背景颜色 */
-            sectionsColor:['#E0EBE7',"#81CDBD",'#55C4C7','#7DACBF','#E0EBE7'],
+            sectionsColor:['#81CDBD'],
             /*显示右侧的导航原点*/
             navigation:true,
             /*弹跳*/
-            easingcss3:'cubic-bezier(0.175,0.885,0.320,1.05)'
+            easingcss3:'cubic-bezier(0.175,0.885,0.320,1.1)',
 
 
         });
